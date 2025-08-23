4f47a60cca27d7f0c92d64935b5431c140f46c7f
Alden Cedric <aldencedric314@gmail.com>
2025-08-23 21:23:52 +0800

    Set window properties

--- StudentRegistrationApplication/Form1.Designer.cs
+++ StudentRegistrationApplication/Form1.Designer.cs
@@ -57,10 +57,15 @@ namespace StudentRegistrationApplication
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.SuspendLayout();
+
+            // Window properties
+            this.Text = "Student Registration Application";
+            this.Size = new Size(600, 500);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.BackColor = Color.White;
         }
 
         #endregion

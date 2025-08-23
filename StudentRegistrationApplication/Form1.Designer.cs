afb147533fdebb2e4a85784950cc612b9936c8f9
Alden Cedric <aldencedric314@gmail.com>
2025-08-23 21:22:13 +0800

    Added variables

--- StudentRegistrationApplication/Form1.Designer.cs
+++ StudentRegistrationApplication/Form1.Designer.cs
@@ -1,4 +1,8 @@
-﻿namespace StudentRegistrationApplication
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StudentRegistrationApplication
 {
     partial class Form1
     {
@@ -6,6 +10,31 @@
         ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+
+        // TextBoxes
+        private TextBox txtStudentName;
+        private TextBox txtStudentId;
+        private TextBox txtAddress;
+
+        // Radio Buttons
+        private RadioButton rbMale;
+        private RadioButton rbFemale;
+
+        // Combo Boxes
+        private ComboBox cmbDay;
+        private ComboBox cmbMonth;
+        private ComboBox cmbYear;
+
+        // Button
+        private Button btnDisplay;
+
+        // Labels
+        private Label lblStudentName;
+        private Label lblStudentId;
+        private Label lblGender;
+        private Label lblDateOfBirth;
+        private Label lblAddress;
+        private Label lblOutput;
 
         /// <summary>
         ///  Clean up any resources being used.
